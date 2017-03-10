75e13b0ad3312f0c7f5e86e6d59e00b21a1c5553
Telomere31 <telomere31+githubdesktop@gmail.com>
2017-03-10 20:15:31 +0100

    Fix JSON result usage

--- MMM-Toulouse-Transports.js
+++ MMM-Toulouse-Transports.js
@@ -116,9 +116,9 @@ Module.register( "MMM-Toulouse-Transports", {
 
             if (this.config.debug) {
                 console.log("JOURNEYS - " + JSON.stringify(this.journeys));
+                this.journeys = JSON.read(this.journeys);
                 //console.log("JOURNEYS - index 0 " + JSON.stringify(this.journeys[0]));
             }
-
 
             if(this.journeys[ journeyIndex ]["journey"] != null) {
                 var departureDateTime = this.journeys[ journeyIndex ].journey.departureDateTime;
